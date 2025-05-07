f5d3d9c1ebd0db829cc9f1792846fae4e391c85a
Giacomo Caria <giacomo.caria@gmail.com>
2025-05-07 13:59:31 +0000

    Increase chunk size to 1024

--- preprocess/preprocess_and_index.py
+++ preprocess/preprocess_and_index.py
@@ -16,7 +16,7 @@ with open(filename, "r", encoding="utf-8") as f:
     book_text = f.read()
 
 # Split into chunks
-splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=50)
 chunks = splitter.split_text(book_text)
 
 # Embed chunks
